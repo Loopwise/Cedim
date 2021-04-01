df3ec2a68502a332f37a544ccd42c2bf030a0bf1
ANDY <60638953+andym2c4@users.noreply.github.com>
2021-04-01 10:24:29 -0500

    Update prueba.cpp

--- C ++ programming/March2021/NotasSesiones/prueba.cpp
+++ C ++ programming/March2021/NotasSesiones/prueba.cpp
@@ -2,6 +2,6 @@
 int main(){
 	std::cout<<"Hola mundo otra vez jiji"<<std::endl;
 	std::cout<<"Mensaje 2"<<std::endl;
-	std::cout<<"mensaje anonimo"<<std::endl;
+	std::cout<<"mensaje deee anonimo"<<std::endl;
 	return 0;
 }
