9bb7a45bafecf88367cf0cdf196601b423f46d92
andymontes <andy_24mc@hotmail.com>
2021-04-01 12:37:58 -0500

    actalizacion 1 abril

--- C ++ programming/March2021/NotasSesiones/prueba.cpp
+++ C ++ programming/March2021/NotasSesiones/prueba.cpp
@@ -2,6 +2,7 @@
 int main(){
 	std::cout<<"Hola mundo otra vez jiji"<<std::endl;
 	std::cout<<"Mensaje 2"<<std::endl;
-	std::cout<<"mensaje deee anonimo"<<std::endl;
+	std::cout<<"Mensaje 3"<<std::endl;
+	std::cout<<"creacion de rama"<<std::endl;
 	return 0;
 }
