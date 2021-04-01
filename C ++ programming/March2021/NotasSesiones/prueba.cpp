8873c511d7b889fe82f544befdd89fe0164288b2
andymontes <andy_24mc@hotmail.com>
2021-04-01 10:23:35 -0500

    programa prueba

--- C ++ programming/March2021/NotasSesiones/prueba.cpp
+++ C ++ programming/March2021/NotasSesiones/prueba.cpp
@@ -2,6 +2,6 @@
 int main(){
 	std::cout<<"Hola mundo otra vez jiji"<<std::endl;
 	std::cout<<"Mensaje 2"<<std::endl;
-	std::cout<<"mensaje 3"<<std::endl;
+	std::cout<<"mensaje anonimo"<<std::endl;
 	return 0;
 }
